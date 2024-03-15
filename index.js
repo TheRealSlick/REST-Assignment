f565623a0b5a4a6cfcab1bae8168599c741cbbd0
TheRealSlick <wadams0107@gmail.com>
2024-03-14 21:06:37 -0700

    finished assignment

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+// Modules/Globals
 const express = require('express')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// Express Settings
 app.set('view engine', 'jsx')
+app.set('views', __dirname + '/views')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers and Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -16,4 +21,5 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
+// Listening for Connections
 app.listen(PORT)
