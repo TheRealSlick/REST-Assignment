e15a303c2ff493025dc4b1e5739a5ced9d3f3a6b
TheRealSlick <wadams0107@gmail.com>
2024-03-17 09:50:03 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -11,7 +11,17 @@ router.get('/new', (req, res) => {
   
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+        req.body.pic = 'http://placekiitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
   
   
